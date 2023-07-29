e508cbeb69786563bf9f0a5a11da6589eb0496f4
memm25ua <memm25@alu.ua.es>
2023-07-29 20:28:40 +0200

    Added color

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 const Hero = () => {
   return (
-    <div className="h-[90vh] w-full relative">
+    <div className="h-[90vh] w-full relative bg-prelude-300">
       <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
         <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
           Summer styles are finally here
@@ -14,20 +14,6 @@ const Hero = () => {
         </p>
         <UnderlineLink href="/store">Explore products</UnderlineLink>
       </div>
-      <Image
-        src="/hero.jpg"
-        loading="eager"
-        priority={true}
-        quality={90}
-        alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
-        className="absolute inset-0"
-        draggable="false"
-        fill
-        sizes="100vw"
-        style={{
-          objectFit: "cover",
-        }}
-      />
     </div>
   )
 }
